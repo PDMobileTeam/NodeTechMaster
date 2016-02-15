11672a3bec1f7774887180d5bfad9b5eb1abc93c
daivp <daivp@aoi-sys.vn>
2016-02-15 14:01:37 +0700

    add dummy data, get data from request body

--- controller/book.js
+++ controller/book.js
@@ -1,4 +1,39 @@
 "use strict";
+var bookDB  = require("../model/books");
 exports.getListBooks = function (req, res) {
-    res.send("list books");
+    bookDB.listBooks(function (err, data) {
+        if (err) {
+            res.json(err)
+        } else {
+            res.json(data)
+        }
+    })
+};
+
+exports.createNewBook = function (req, res) {
+    bookDB.createBook(req.body, function (err, data) {
+        if (err) {
+            res.json(err)
+        } else {
+            res.json(data)
+        }
+    })
+};
+
+exports.deleteMultiBooks = function (req, res) {
+    res.json("delete")
+};
+
+exports.updateMultiBooks = function (req, res) {
+    res.json("update")
 }
+
+exports.getBook = function (req, res) {
+    bookDB.getBook(req.params.id, function (err, data) {
+        if (err) {
+            res.json(err)
+        } else {
+            res.json(data)
+        }
+    })
+};
--- server.js
+++ server.js
@@ -2,10 +2,16 @@ var express = require("express");
 var app = express();
 var morgan = require("morgan"); //logger
 var bookRoute = require("./route/book");
+var bodyParser = require('body-parser')
 
 app.use(morgan("dev"));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use("/", bookRoute);
+// parse application/json
+app.use(bodyParser.json())
+
+app.use("/api", bookRoute);
 
 app.listen(3000);
-console.log("Server run on port 3000");
+console.log("Server runs on port 3000");
