43fed348dddc2fd5674d990931d2041734c0bd32
daivp <daivp@aoi-sys.vn>
2016-02-16 13:48:30 +0700

    function get list, create new book with MongoDB

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var morgan = require("morgan"); //logger
 var bookRoute = require("./route/book");
 var bodyParser = require('body-parser')
 var errors = require("./errors")
+var mongoose = require("mongoose")
+var connString = "mongodb://localhost:27017/demoApi"
+
+mongoose.connect(connString)
+mongoose.connection.on("connected", function(){
+    console.log("Connected mongodb")
+})
+
+global.Book = require("./model/books");
 
 app.use(morgan("dev"));
 // parse application/x-www-form-urlencoded
